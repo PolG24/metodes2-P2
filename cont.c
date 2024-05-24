30264a8620868de3f34a15ece115f0b0c3880040
PolG24 <pgabalsa7@alumnes.ub.edu>
2024-05-24 11:28:54 +0200

    Plotting with points. Renamed file. Commented name and surnames.

--- cont.c
+++ cont.c
@@ -1,47 +1,189 @@
+// Pol Gabaldon Salvador
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
-#define MAX_POINTS 1000 // Maximum number of points on the curve
+#define TOL 1e-10
 
-double g(double y) {
-    return -4*y*y*y*y + 3*y*y + 0.2*y + 0.1;
+// Define the function f(x, y)
+double f(double x, double y)
+{
+    return -4 * pow(y, 4) - 6 * pow(x, 2) * pow(y, 2) + 11 * x * pow(y, 2) + 3 * pow(y, 2) - 6 * pow(x, 4) + 21 * pow(x, 3) - 19 * pow(x, 2) + 0.2 * y + 0.1;
 }
 
-double dg(double y) {
-    return -16*y*y*y + 6*y + 0.2;
+// Partial derivative of f with respect to x
+double fx(double x, double y)
+{
+    return -12 * x * pow(y, 2) + 11 * pow(y, 2) - 24 * pow(x, 3) + 63 * pow(x, 2) - 38 * x;
 }
 
-// void corrector(double h, int nc, double err, int nn) {
-//     double x[MAX_POINTS];
-//     int i;
+// Partial derivative of f with respect to y
+double fy(double x, double y)
+{
+    return -16 * pow(y, 3) - 12 * pow(x, 2) * y + 22 * x * y + 6 * y + 0.2;
+}
 
-//     // Initialization
-//     x[0] = 0.0;
+// Function to calculate the determinant of a 2x2 matrix
+double determinant_2x2(double matrix[4])
+{
+    return matrix[0] * matrix[3] - matrix[1] * matrix[2];
+}
 
-//     // Predictor-Corrector Method
-//     for (i = 1; i < nc && i < MAX_POINTS; i++) {
-//         x[i] = x[i - 1] + h; // Predictor step
-//         int j;
-//         for (j = 0; j < nn; j++) {
-//             double f_xi = f(x[i]);
-//             double df_xi = df(x[i]);
-//             double dx = -f_xi / df_xi; // Corrector step
-//             x[i] += dx;
-//             if (fabs(dx) < err) break; // Stop iteration if error is within tolerance
-//         }
-//         if (i == nc - 1) break; // Reached maximum number of points
-//     }
+// Newton method to find y for a given x such that f(x, y) = 0
+double newton_method(double (*f)(double, double), double (*df)(double, double), double x0, double y_start, double err)
+{
+    double y = y_start;
+    while (fabs(f(x0, y)) > err)
+    {
+        // In our very particular case there is no need to check that this denominator is non-zero, so
+        // we don't to negligibly improve efficiency. This could cause problems for functions, x0 and y_start
+        // different to the ones used.
+        y -= f(x0, y) / df(x0, y);
+    }
+    return y;
+}
 
-//     // Output the points
-//     printf("Points on the curve:\n");
-//     for (int j = 0; j < i; j++) {
-//         printf("%f\n", x[j]);
-//     }
-// }
+// Two dimensional Newton Method
+double two_d_newton_method(double (*f)(double, double), double (*fx)(double, double),
+                           double (*fy)(double, double), double *pred, double *prev, double h, double err, int nn)
+{
+    int i = 0;
+    double DF[4], inverse[4], result[2], det;
 
-int main(int argc, char *argv[]) {
-    if (argc != 5) {
+    while (i < nn)
+    {
+        // Compute the Jacobian matrix DF
+        DF[0] = fx(pred[0], pred[1]);
+        DF[1] = fy(pred[0], pred[1]);
+        DF[2] = 2 * (pred[0] - prev[0]);
+        DF[3] = 2 * (pred[1] - prev[1]);
+
+        // Compute the determinant of DF
+        det = determinant_2x2(DF);
+
+        // Check whether the determinant is too close to zero
+        // (division by zero issues could arise)
+        if (fabs(det) < TOL)
+        {
+            printf("The determinant is too close to zero!\n");
+            return -1;
+        }
+
+        // Compute the inverse of the Jacobian matrix DF
+        inverse[0] = DF[3] / det;
+        inverse[1] = -DF[1] / det;
+        inverse[2] = -DF[2] / det;
+        inverse[3] = DF[0] / det;
+
+        // Compute the function F at the current prediction
+        result[0] = f(pred[0], pred[1]);
+        result[1] = pow((pred[0] - prev[0]), 2) + pow((pred[1] - prev[1]), 2) - h * h;
+
+        // Check for convergence
+        if (sqrt(result[0] * result[0] + result[1] * result[1]) < err)
+        {
+            return 0; // Converged successfully
+        }
+
+        // Update the current point using the Newton-Raphson step
+        pred[0] = pred[0] - (inverse[0] * result[0] + inverse[1] * result[1]);
+        pred[1] = pred[1] - (inverse[2] * result[0] + inverse[3] * result[1]);
+
+        i++;
+    }
+
+    printf("Maximum iterations (nn) reached without convergence.\n");
+    return -1;
+}
+
+void tangent_vector(double (*fx)(double, double), double (*fy)(double, double), double x, double y, double *tangent_x, double *tangent_y)
+{
+    // Calculate the gradient
+    *tangent_x = fx(x, y);
+    *tangent_y = fy(x, y);
+
+    // Calculate an orthogonal vector to the gradient
+    double temp = *tangent_x;
+    *tangent_x = -*tangent_y;
+    *tangent_y = temp;
+
+    // Compute the norm of the orthogonal vector
+    double norm = sqrt(*tangent_x * *tangent_x + *tangent_y * *tangent_y);
+
+    // Normalize the orthogonal vector to get the unit tangent vector
+    *tangent_x = *tangent_x / norm;
+    *tangent_y = *tangent_y / norm;
+}
+
+// Predictor-corrector method
+void predictor_corrector(double (*f)(double, double),
+                         double (*fx)(double, double),
+                         double (*fy)(double, double),
+                         double h, int nc, double err, int nn, double *points, bool verbose)
+{
+    // Use prev to store the previous point, pred to store the prediction
+    double prev[2], pred[2];
+
+    // The first point is found taking x = 0 and using Newton's method to find y
+    prev[0] = 0.0;
+    prev[1] = newton_method(f, fy, 0.0, 0.0, err);
+    if (verbose)
+    {
+        printf("Starting point: (%e, %e)\n", prev[0], prev[1]);
+    }
+
+    // Save the first point to the points array
+    points[0] = prev[0];
+    points[1] = prev[1];
+
+    double tangent_x, tangent_y;
+
+    for (int i = 1; i < nc; ++i)
+    {
+        // Find the tangent vector at (x, y)
+        tangent_vector(fx, fy, prev[0], prev[1], &tangent_x, &tangent_y);
+        if (verbose)
+        {
+            printf("The number %d unitary tangent vector is: (%e, %e)\n", i, tangent_x, tangent_y);
+        }
+
+        // Use the tangent vector to predict the next point
+        pred[0] = prev[0] + h * tangent_x;
+        pred[1] = prev[1] + h * tangent_y;
+        if (verbose)
+        {
+            printf("Initial prediction: (%e, %e)\n", pred[0], pred[1]);
+        }
+
+        // Use the two dimensional Newton method to correct the prediction
+        two_d_newton_method(f, fx, fy, pred, prev, h, err, nn);
+        if (verbose)
+        {
+            printf("Adjusted prediction: (%e, %e)\n", pred[0], pred[1]);
+        }
+
+        // Update the previous prediction for the next iteration
+        prev[0] = pred[0];
+        prev[1] = pred[1];
+
+        // Save the predicted point to the points array
+        points[2 * i] = pred[0];
+        points[2 * i + 1] = pred[1];
+
+        // Check if we are close to the starting point (closed curve)
+        if (i > 1 && sqrt(pow(pred[0] - points[0], 2) + pow(pred[1] - points[1], 2)) < h)
+        {
+            printf("Closed curve completed\n");
+            break;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 5)
+    {
         printf("Usage: %s <h> <nc> <err> <nn>\n", argv[0]);
         return 1;
     }
@@ -50,22 +192,48 @@ int main(int argc, char *argv[]) {
     int nc, nn;
 
     // Convert command line arguments to appropriate data types
-    h = atof(argv[1]);
-    nc = atoi(argv[2]);
-    err = atof(argv[3]);
-    nn = atoi(argv[4]);
+    h = atof(argv[1]);   // Step size
+    nc = atoi(argv[2]);  // Max number of points
+    err = atof(argv[3]); // Maximum allowed error
+    nn = atoi(argv[4]);  // Number of iterations in corrector method
 
-    int i;
-
-    // Initialization
-    double y = 0.0;
-
-    // Find the starting point with the Newton method
-    while (fabs(g(y)) > err) {
-        y += -(g(y) / dg(y));
+    if (h <= 0 || nc <= 0 || err <= 0 || nn <= 0)
+    {
+        printf("All input parameters must be positive.\n");
+        return 1;
     }
 
-    printf("Starting point: (0, %e).\n", y);
+    // Allocate dynamic memory to store the points in case we wish to compute a fine curve
+    double *points = (double *)malloc(2 * nc * sizeof(double));
+    if (points == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // Run the predictor corrector algorithm
+    bool verbose = false;
+    predictor_corrector(f, fx, fy, h, nc, err, nn, points, verbose);
+
+    // Write points to file
+    FILE *file = fopen("curve_points.txt", "w");
+    if (file == NULL)
+    {
+        printf("File opening failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < nc; ++i)
+    {
+        // All points have been mallocked to zeroes, if the curve is closed before we
+        // fill the points array we want to ignore these zeroes. This assumes our curve
+        // does not approach (0, 0), which is true in our case.
+        if (points[2 * i] == 0 && points[2 * i + 1] == 0) break;
+        fprintf(file, "%lf %lf\n", points[2 * i], points[2 * i + 1]);
+    }
+
+    fclose(file);
+    free(points);
 
     return 0;
 }
--- plot_curve.gnuplot
+++ plot_curve.gnuplot
@@ -1,3 +1,4 @@
+# Pol Gabaldon Salvador
 # plot_curve.gnuplot
 set terminal pngcairo size 800,600 enhanced font 'Verdana,10'
 set output 'curve_plot.png'
@@ -7,4 +8,4 @@ set xlabel "x"
 set ylabel "y"
 set grid
 
-plot 'curve_points.txt' using 1:2 with linespoints title 'Curve'
+plot 'curve_points.txt' using 1:2 with points pointtype 7 pointsize 0.2 title 'Curve'
